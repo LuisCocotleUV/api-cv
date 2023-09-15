b51b148522910dc3bb067c88d3ca8984bfedbc60
Cocotle <luisdavidcocoyuv@gmail.com>
2023-09-15 08:51:17 -0600

    update api info

--- index.js
+++ index.js
@@ -12,9 +12,24 @@ app.get('/', (req, res, next) => {
 	res.send('node express api <br> by adsoft');
 });
 
-
 app.get('/header', (req, res, next) => {
   res.sendfile("assets/header.json");
+});
+
+app.get('/work-experience', (req, res, next) =>{
+	res.sendfile("assets/work-experience.json");
+});
+
+app.get('/skills', (req, res, next) =>{
+	res.sendfile("assets/skills.json");
+});
+
+app.get('/certifications', (req, res, next) =>{
+	res.sendfile("assets/certifications.json");
+});
+
+app.get('/interests', (req, res, next) =>{
+	res.sendfile("assets/interests.json");
 });
 
 app.listen(port,  () => 
